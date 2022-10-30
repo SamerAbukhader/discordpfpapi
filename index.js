a04f1ae43ba5278249d44987b9017e402d02b834
SamerAbukhader <sakhader_2003@hotmail.com>
2022-10-30 13:42:04 +0800

    idx

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function main(id, token){
     return link
 }
 
-app.post('/', async (req, res)=>{
+app.post('/api/getpfp', async (req, res)=>{
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Max-Age", "1800");
